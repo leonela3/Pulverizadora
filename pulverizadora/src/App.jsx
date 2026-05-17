cdb3f0cf2e95737201193f1ce8119893b362f78e
leonela <leonelancalvo@gmail.com>
2026-05-17 11:24:30 -0300

    Primer proyecto React + Firebase

---
+++ pulverizadora/src/App.jsx
@@ -0,0 +1,12 @@
+import "./firebase";
+
+function App() {
+  return (
+    <div>
+      <h1>Pulverizaciones La Julia 🚜</h1>
+      <p>Firebase conectado correctamente</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ pulverizadora/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvMITG0kadF-hZoacHCkN3lrwvMsYncRA",
+  authDomain: "pulverizadora-3305b.firebaseapp.com",
+  projectId: "pulverizadora-3305b",
+  storageBucket: "pulverizadora-3305b.firebasestorage.app",
+  messagingSenderId: "191727809731",
+  appId: "1:191727809731:web:e93ccd04b8998074121e76"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
